9b96e9ad42aad3593d937508bec4911bd20987a0
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-21 06:34:20 +0000

    add support

---
+++ support/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def faq_view(request):
+    return render(request, 'support/faq.html')
+
+
+def contact_view(request):
+    return render(request, 'support/contact.html')
+
+
+def about_view(request):
+    return render(request, 'support/about.html')
+
+
+def team_view(request):
+    return render(request, 'support/team.html')
